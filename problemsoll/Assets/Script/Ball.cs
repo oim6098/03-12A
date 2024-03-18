5ac13edc7f0386027e0da29b6399940fb6b367c9
oim6098 <lulm6098@naver.com>
2024-03-18 16:40:33 +0900

    [Good]

---
+++ problemsoll/Assets/Script/Ball.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        // 빨간색 박스에 도달하면 공 삭제
+        if (other.gameObject.CompareTag("RedBox"))
+        {
+            Debug.Log("충돌됨");
+            // Ball을 삭제
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ problemsoll/Assets/Script/Queue.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class Queue : MonoBehaviour
+{
+    private class Bullet
+    {
+        public GameObject gameObject;
+        public bool isActive;
+    }
+
+    private Queue<Bullet> bulletQueue = new Queue<Bullet>();
+    public GameObject bulletPrefab;
+    public GameObject greenBox;
+    public GameObject ballPrefab;
+
+    void Start()
+    {
+        // 초기화 시 총알을 10개 생성하여 Queue에 넣음
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.SetActive(false);
+            Bullet bullet = new Bullet
+            {
+                gameObject = bulletObject,
+                isActive = false
+            };
+            bulletQueue.Enqueue(bullet);
+        }
+    }
+
+    void Update()
+    {
+        // 좌클릭하여 총알 발사
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBullet();
+        }
+    }
+
+    void ShootBullet()
+    {
+        // Queue에서 총알을 꺼내서 발사
+        if (bulletQueue.Count > 0)
+        {
+            Bullet bullet = bulletQueue.Dequeue();
+            bullet.isActive = true;
+            bullet.gameObject.SetActive(true);
+            bullet.gameObject.transform.position = greenBox.transform.position; // 초록색 박스의 위치로 설정
+            Rigidbody rb = bullet.gameObject.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.right * 10f; // 오른쪽으로 총알 발사
+        }
+    }
+}
